b7d04fbd1da8ce9e4f04761e87e194c8eb2dbf67
Wangjin1 <253304729@qq.com>
2020-09-11 08:28:14 +0800

    m提交xshell工具

---
+++ mayiketang/7/jvm/jvm03/src/main/java/com/taotao/jvm03/jvm04/Test002.java
@@ -0,0 +1,12 @@
+package com.taotao.jvm03.jvm04;
+
+/**
+ *@author tom
+ *Date  2020/9/9 0009 9:11
+ *
+ */
+public class Test002 {
+    public static void main(String[] args) {
+        Test002 test002=new Test002();
+    }
+}
---
+++ mayiketang/7/jvm/jvm03/src/main/java/com/taotao/jvm03/jvm04/UserMayiEntry.java
@@ -0,0 +1,20 @@
+package com.taotao.jvm03.jvm04;
+
+/**
+ *@author tom
+ *Date  2020/9/11 0011 7:19
+ *
+ */
+
+public class UserMayiEntry {
+    private  String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public UserMayiEntry setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+}
