31133564ae5966979208a6e35f2f86c4e9ad96b3
Wangjin1 <253304729@qq.com>
2020-10-03 07:23:03 +0800

    初始化

---
+++ mayiketang/7/hashmap/hashmap001/src/main/java/com/taotao/hashmap001/Hashmap001Application.java
@@ -0,0 +1,13 @@
+package com.taotao.hashmap001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hashmap001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hashmap001Application.class, args);
+    }
+
+}
