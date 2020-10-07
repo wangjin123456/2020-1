c71c62a6591fcdda1923f629a8ee2d029fa4f279
Wangjin1 <253304729@qq.com>
2020-10-07 09:26:19 +0800

    提交concurrenthashmap

---
+++ mayiketang/7/hashmap/hashmap001/src/main/java/com/taotao/hashmap001/currenthashmap/Test013.java
@@ -0,0 +1,17 @@
+package com.taotao.hashmap001.currenthashmap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *@author tom
+ *Date  2020/10/3 0003 9:53
+ *并发的jdk1.8 ConcurrentHashmap
+ */
+public class Test013 {
+
+    public static void main(String[] args) {
+        ConcurrentHashMap<Object,Object> concurrentHashMap=new ConcurrentHashMap<>();
+        concurrentHashMap.put("tom","tom");
+        System.out.println(16>>>2);
+    }
+}
