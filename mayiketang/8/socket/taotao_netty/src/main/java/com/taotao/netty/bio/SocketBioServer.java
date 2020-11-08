ecbd2e983400d33ef4da01dcf21271bbe48f4212
Wangjin1 <253304729@qq.com>
2020-11-08 11:59:02 +0800

    bio

---
+++ mayiketang/8/socket/taotao_netty/src/main/java/com/taotao/netty/bio/SocketBioServer.java
@@ -0,0 +1,49 @@
+package com.taotao.netty.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *@author tom
+ *Date  2020/11/8 0008 10:37
+ *
+ */
+public class SocketBioServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket=new ServerSocket(9001);
+        while (true){
+            System.out.println("服务端正在等待连接中....");
+            //阻塞方法 如果没有客户端与服务器端建立连接时，该方法会阻塞等待
+            final Socket socket=serverSocket.accept();
+            System.out.println("有客户端和我连接了");
+            //如果不使用异步线程处理接受io操作的情况下，有可能会阻塞等待 无法接受新的连接请求;
+      new Thread(()->{
+         try {
+             handler(socket);
+         }catch (Exception e){
+             e.printStackTrace();
+         }
+      }).start();
+
+        }
+
+    }
+
+    private static void handler(Socket socket) throws IOException {
+        System.out.println("线程id="+Thread.currentThread());
+        byte[] bytes=new byte[1024];
+        System.out.println("开始read..");
+        //接收客户端的数据，如果没有读取到客户端数据时，该方法也会阻塞
+        int read=socket.getInputStream().read(bytes);
+        System.out.println("read结束");
+        if(read!=-1){
+            System.out.println("接收到客户端的数据："+new String(bytes,0,read));
+            System.out.println("线程id=="+Thread.currentThread().getId());
+        }
+        socket.getOutputStream().write("效果演示完毕".getBytes());
+        socket.getOutputStream().flush();
+    }
+
+
+}
